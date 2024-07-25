d53a0c08848c04f197a9729d8efcb010922d90bd
rewind122 <luvyoudear12@gmail.com>
2024-07-25 18:03:23 +0900

    [환경설정] eslint, prettier 등 설정 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
 ];
