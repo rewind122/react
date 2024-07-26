22105b91fe7e21f538d69f0c0732e93550fb8872
rewind122 <luvyoudear12@gmail.com>
2024-07-26 15:04:33 +0900

    [수업] 아바타 컴포넌트 완료

---
+++ 05-react-component-with-props/src/pages/AvatarListPage.js
@@ -0,0 +1,15 @@
+import { createElement as h } from 'https://esm.sh/react';
+import Avatar from '../components/Avatar.js';
+
+function AvatarListPage() {
+  return h(
+    'ul',
+    {
+      className: 'AvatarList',
+    },
+    h('li', null, h(Avatar, { name: '야무', photo: 'man-02.jpg', status: 'online' })),
+    h('li', null, h(Avatar, { name: '범쌤', photo: 'man-04.jpg', status: 'away' }))
+  );
+}
+
+export default AvatarListPage;
