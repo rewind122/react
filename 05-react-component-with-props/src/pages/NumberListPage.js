629e7427eccd743852a33bbc6153aa420fb639c2
rewind122 <luvyoudear12@gmail.com>
2024-07-26 13:38:53 +0900

    [수정] NumberListPage.js 수정

--- 05-react-component-with-props/src/pages/NumberListPage.js
+++ 05-react-component-with-props/src/pages/NumberListPage.js
@@ -4,9 +4,7 @@ import NumberList from '../components/NumberList.function.js';
 function NumberListPage() {
   // NumberList 컴포넌트 -> 리액트 엘리먼트 생성
   // NumberList 컴포넌트에 속성(props) 전달
-  return (
-    React.createElement(NumberList, { count: 9 });
-  )
+  return React.createElement(NumberList, { count: 9 });
 }
 
 export default NumberListPage;
