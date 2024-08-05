bedffb74b38b3d36f32b6c4e3a15c144ddd84134
rewind122 <luvyoudear12@gmail.com>
2024-08-06 08:56:03 +0900

    [수업]

---
+++ 12-react-component-states/src/Playground.jsx
@@ -0,0 +1,15 @@
+// import TermAndConditions from '@/components/TermAndCondition';
+// import ExpandableText from "@/components/ExpandableText";
+// import ExpandableText from "@/components/ExpandableText.class";
+// import AvatarListPage from '@/pages/AvatarListPage';
+import Counter from '@/components/Counter';
+
+function Playground() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default Playground;
