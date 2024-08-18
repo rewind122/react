c31d4347dd0acea3f95bd0dcd307d9bf685cd6ba
rewind122 <luvyoudear12@gmail.com>
2024-08-19 08:47:43 +0900

    [수업] 학습 자료 다운로드

--- 15-effect-sync-external-system/src/@euid-router/RouterProvider/types.js
+++
@@ -1,9 +0,0 @@
-import { arrayOf, element, exact, string } from 'prop-types';
-
-export const RouteType = exact({
-  path: string.isRequired,
-  element: element.isRequired,
-  title: string.isRequired,
-});
-
-export const RouteListType = arrayOf(RouteType);
